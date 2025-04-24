ed77d3ee4f8a54fccb73c9d6b440d12727956236
Aluno <Aluno@politecnico.local>
2025-04-23 21:26:54 -0300

    icone de editar

--- clientes/urls.py
+++ clientes/urls.py
@@ -4,6 +4,6 @@ from .views import ClienteView, ClienteAddView, ClienteUpdateView, ClienteDelete
 urlpatterns = [
     path('clientes', ClienteView.as_view(), name='clientes'),
     path('cliente/adicionar/', ClienteAddView.as_view(), name='cliente_adicionar'),
-    path('<int:pk>cliente/editar/', ClienteUpdateView.as_view(), name='cliente_editar'),
-    path('<int:pk>cliente/apagar/', ClienteDeleteView.as_view(), name='cliente_apagar'),
+    path('<int:pk>/cliente/editar/', ClienteUpdateView.as_view(), name='cliente_editar'),
+    path('<int:pk>/cliente/apagar/', ClienteDeleteView.as_view(), name='cliente_apagar'),
 ]
